bf42a6817e6fc4d46a49c1ab6e891a60c287fa25
Manish J <manish.in.java@gmail.com>
2017-09-10 20:42:01 +0530

    Updated the DataTables configuration to prevent pagination controls from appearing when the number of records is below the pagination threshold.

--- Python/PetClinic/PetClinic/static/scripts/doctor.js
+++ Python/PetClinic/PetClinic/static/scripts/doctor.js
@@ -3,5 +3,16 @@ $(document).ready(function () {
         "ajax": "/doctor/page"
         , "processing": true
         , "serverSide": true
+        , "fnDrawCallback": function (settings) {
+            console.log();
+            var pager = $(settings.nTableWrapper).find(".dataTables_paginate")
+            if (settings._iDisplayLength == -1
+                || settings._iDisplayLength > settings.fnRecordsDisplay()
+                || settings._iDisplayLength > settings.aoData.length) {
+                pager.hide();
+            } else {
+                pager.show()
+            }
+        }
     });
 });
